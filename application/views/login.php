3a92bb8aca6a1993bdcafde4c935f099b1caf279
davidmarquezherreros <davidmarquezherreros@gmail.com>
2017-01-10 19:55:04 +0100

    Añadido favicon.ico a la pagina de login

--- application/views/login.php
+++ application/views/login.php
@@ -3,11 +3,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
-	<link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.min.css"); ?>" />
-</head>
+<?php
+	$this->load->view('inc/head.php');
+ ?>
 <body>
 	<script type="text/javascript" src="<?php echo base_url("assets/js/jquery.min.js"); ?>"></script>
 	<script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.min.js"); ?>"></script>
