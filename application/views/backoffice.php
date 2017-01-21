26890a3744177ca066beba8725bce95d83972e7c
davidmarquezherreros <davidmarquezherreros@gmail.com>
2017-01-21 18:36:38 +0100

    Back office mas o menos

--- application/views/backoffice.php
+++ application/views/backoffice.php
@@ -3,9 +3,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?><!DOCTYPE html>
 <html lang="en">
 <?php
+foreach($css_files as $file): ?>
+    <link type="text/css" rel="stylesheet" href="<?php echo $file; ?>" />
+
+<?php endforeach; ?>
+<?php foreach($js_files as $file): ?>
+
+    <script src="<?php echo $file; ?>"></script>
+<?php endforeach; ?>
+
+<?php
 	$this->load->view('inc/head.php');
  ?>
- 
+
 <body>
 	<div id="container">
 		<div id="menu">
