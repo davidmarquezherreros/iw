7e654b7bf283f5b196e00ecfbea2d7a32aeb481d
davidmarquezherreros <davidmarquezherreros@gmail.com>
2017-01-21 12:06:48 +0100

    Creado los metodos inciar y cerrar sesion

--- application/controllers/Sesion.php
+++ application/controllers/Sesion.php
@@ -2,37 +2,74 @@
    defined('BASEPATH') OR exit('No direct script access allowed');
    class Sesion extends CI_Controller {
 
+     function __construct() {
+        parent::__construct();
+        //loading libraries
+        $this->load->library('session');
+        $this->load->helper('form');
+
+        // loading models
+        $this->load->model("usuario_m", '', TRUE);
+
+     }
       public function index() {
-         //loading libraries
-         $this->load->library('session');
-         $this->load->helper('form');
-         $this->load->view('login');
+         //loading view
+         // Si el usuario ya se ha logueado, que entre al index
+         if($this->session->userdata('usuarioLogueado')) {
+           echo "<script>  alert('Usuario logueado: " . $this->session->userdata('usuarioLogueado') . "');
+                   window.location.href = '/iw/index.php/home/';
+                </script>"
+           ;
+         }
+         else{
+           $this->load->view('login');
+         }
+
       }
 
-      public function unset_session_data() {
-         //loading session library
-         $this->load->library('session');
-
+      public function logout() {
          //removing session data
-         $this->session->unset_userdata('name');
+         $this->session->unset_userdata('usuarioLogueado');
          $this->load->view('login');
       }
 
       public function login(){
-        if (count($_POST)>0) //Solo se ejecutará si se han enviado datos por el formulario
-        {
-            // echo $_POST['username'];
-            $username = $this->input->post('username');
-            $password = $this->input->post('password');
-            if(true){ // El usuario puede acceder a la web
-
-            }
-            else{ // El usuario no puede acceder a la web
-              $this->index();
-            }
+        // Si el usuario ya se ha logueado, que entre al index
+        if($this->session->userdata('usuarioLogueado')) {
+          echo "<script>  alert('Usuario logueado: " . $this->session->userdata('usuarioLogueado') . "');
+                  window.location.href = '/iw/index.php/home/';
+               </script>"
+          ;
         }
         else{
-            $this->index();
+          if (count($_POST)>0) //Solo se ejecutará si se han enviado datos por el formulario
+          {
+              // echo $_POST['username'];
+              $username = $this->input->post('username');
+              $password = $this->input->post('password');
+              $usuario = $this->usuario_m->login($username, $password);
+
+              if(count($usuario)==1){ // El usuario puede acceder a la web
+                //echo $usuario[0]->Username;
+                //echo $usuario[0]->Password;
+                $this->session->set_userdata('usuarioLogueado', $usuario[0]->Username);
+                $this->session->set_flashdata('login', 'Bienvenid@ ' . $usuario[0]->Username . '!');
+
+                echo "<script>
+                        window.location.href = '/iw/index.php/home/';
+                     </script>"
+                ;
+
+              }
+              else{ // El usuario no puede acceder a la web
+                echo "<script>  alert('Credenciales incorrectas intentelo de nuevo');
+                        window.location.href = '/iw/index.php/sesion/login';
+                     </script>";
+              }
+          }
+          else{
+              $this->index();
+          }
         }
       }
    }
