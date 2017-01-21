617f0e92fa481944bb62faf8c3e47ec722bb96cb
davidmarquezherreros <davidmarquezherreros@gmail.com>
2017-01-21 12:16:14 +0100

    Cambiado el menu

--- application/views/inc/menu.php
+++ application/views/inc/menu.php
@@ -5,7 +5,12 @@
     </div>
     <ul class="nav navbar-nav">
       <li class="active"><a href='<?php echo site_url('home')?>'>Home</a></li>
-      <li class="active"><a href='<?php echo site_url('sesion')?>'>Iniciar sesión</a></li>
+      <?php
+          $sesion=$this->session->userdata('usuarioLogueado');
+          if($sesion==""){
+              echo "<li class=\"active\"><a href='".site_url('sesion')."'>Iniciar sesión</a></li>";
+          }
+      ?>
       <li class="active"><a href='<?php echo site_url('home')?>'>Mi ebay</a></li>
       <li class="active"><a href='<?php echo site_url('home')?>'>Buscar</a></li>
       <li class="active"><a href='<?php echo site_url('home')?>'><span class="glyphicon glyphicon-shopping-cart" aria-hidden="true"></span></a></li>
