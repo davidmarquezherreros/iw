6b87bdffe89251cedd47c32748159ab64c1bd05d
davidmarquezherreros <davidmarquezherreros@gmail.com>
2017-01-22 09:17:44 +0100

    BackOffice terminado, faltaria hacer comprobaciones antes de hacer las operaciones

--- application/controllers/BackOffice.php
+++ application/controllers/BackOffice.php
@@ -28,20 +28,123 @@ class BackOffice extends CI_Controller {
 			 	 	/* ------------------ */
 			 		$this->load->library('grocery_CRUD');
 					$this->load->model('grocery_CRUD_model');
+					if($this->session->userdata('admin')==false){
+						echo "<script>  alert('No eres administrador!');
+										window.location.href = '/iw/index.php/home/';
+								 </script>"
+						;
+					}
 
 	 }
 
 		public function index()
 		{
-			redirect("backoffice/usuarios");
+			if($this->session->userdata('admin')==false){
+				echo "<script>  alert('No eres administrador!');
+								window.location.href = '/iw/index.php/home/';
+						 </script>"
+				;
+			}
+			else{
+				redirect("backoffice/usuarios");
+			}
 		}
 
 		public function usuarios(){
 			$crud = new grocery_CRUD();
-			$crud->set_theme("datatables");
+			$crud->set_theme("flexigrid");
 			$crud->set_table('Usuario');
 			$crud->set_subject('Usuario');
-			$crud->columns('Usuario','Contraseña','Email','Telefono','Admin');
+			$crud->columns('id','Username','Password','Email','Telefono','admin');
+			$crud->change_field_type('Admin', 'true_false');
+			$output = $crud->render();
+			$this->load->view('backoffice', $output);
+		}
+
+		public function articulos(){
+			$crud = new grocery_CRUD();
+			$crud->set_theme("flexigrid");
+			$crud->set_table('Articulos');
+			$crud->set_subject('Articulo');
+			$crud->columns('id','Nombre','Descripcion','Precio','FK_Seccion','FK_Sub_Seccion');
+			$output = $crud->render();
+			$this->load->view('backoffice', $output);
+		}
+
+		public function imagenes(){
+			$crud = new grocery_CRUD();
+			$crud->set_theme("flexigrid");
+			$crud->set_table('Imagenes');
+			$crud->set_subject('Imagen');
+			$crud->columns('id','titulo','imagen','FK_Imagen_Articulo');
+			$output = $crud->render();
+			$this->load->view('backoffice', $output);
+		}
+
+		public function opiniones(){
+			$crud = new grocery_CRUD();
+			$crud->set_theme("flexigrid");
+			$crud->set_table('Opinion');
+			$crud->set_subject('Opinion');
+			$crud->columns('id','mensaje','fecha','FK_Usuario');
+			$output = $crud->render();
+			$this->load->view('backoffice', $output);
+		}
+
+		public function secciones(){
+			$crud = new grocery_CRUD();
+			$crud->set_theme("flexigrid");
+			$crud->set_table('Seccion');
+			$crud->set_subject('Seccion');
+			$crud->columns('id','Nombre');
+			$output = $crud->render();
+			$this->load->view('backoffice', $output);
+		}
+
+		public function subsecciones(){
+			$crud = new grocery_CRUD();
+			$crud->set_theme("flexigrid");
+			$crud->set_table('Sub_seccion');
+			$crud->set_subject('Sub seccion');
+			$crud->columns('id','Nombre','FK_Seccion');
+			$output = $crud->render();
+			$this->load->view('backoffice', $output);
+		}
+
+		public function pedidos(){
+			$crud = new grocery_CRUD();
+			$crud->set_theme("flexigrid");
+			$crud->set_table('Pedido');
+			$crud->set_subject('Pedido');
+			$crud->columns('numpedido','fecha','FK_Usuario');
+			$output = $crud->render();
+			$this->load->view('backoffice', $output);
+		}
+
+		public function lineapedidos(){
+			$crud = new grocery_CRUD();
+			$crud->set_theme("flexigrid");
+			$crud->set_table('Linea_pedido');
+			$crud->set_subject('Linea pedido');
+			$crud->columns('id','cantidad','importe','FK_Articulo','FK_Pedido');
+			$output = $crud->render();
+			$this->load->view('backoffice', $output);
+		}
+
+		public function direcciones(){
+			$crud = new grocery_CRUD();
+			$crud->set_theme("flexigrid");
+			$crud->set_table('Direcciones');
+			$crud->set_subject('Direccion');
+			$crud->columns('id','Pais','Direccion','CodigoPostal','CiudadAutonoma','Telefono', 'FK_Usuario');
+			$output = $crud->render();
+			$this->load->view('backoffice', $output);
+		}
+		public function articulosusuario(){
+			$crud = new grocery_CRUD();
+			$crud->set_theme("flexigrid");
+			$crud->set_table('Articulo_Usuario');
+			$crud->set_subject('Articulo usuario');
 			$output = $crud->render();
 			$this->load->view('backoffice', $output);
 		}
--- application/views/backoffice.php
+++ application/views/backoffice.php
@@ -11,11 +11,13 @@ foreach($css_files as $file): ?>
 
     <script src="<?php echo $file; ?>"></script>
 <?php endforeach; ?>
-
-<?php
-	$this->load->view('inc/head.php');
- ?>
-
+<head>
+	<meta charset="utf-8">
+	<title>ebay</title>
+	<link rel="icon" href="<?php echo base_url("assets/favicon.ico"); ?>" type="image/gif">
+	<script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.min.js"); ?>"></script>
+	<link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.min.css"); ?>" />
+</head>
 <body>
 	<div id="container">
 		<div id="menu">
@@ -23,13 +25,25 @@ foreach($css_files as $file): ?>
 				$this->load->view('inc/menu.php');
 			 ?>
 		</div>
-		<div id="secciones">
-		</div>
 		<div id="backoffice" style="min-width=480px; margin: auto; width: 80%;">
+      <nav class="navbar navbar-default">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href='<?php echo site_url('backoffice/Usuarios')?>'>Usuarios</a></li>
+            <li class="active"><a href='<?php echo site_url('backoffice/direcciones')?>'>Direcciones</a></li>
+            <li class="active"><a href='<?php echo site_url('backoffice/secciones')?>'>Secciones</a></li>
+            <li class="active"><a href='<?php echo site_url('backoffice/subsecciones')?>'>Sub secciones</a></li>
+            <li class="active"><a href='<?php echo site_url('backoffice/pedidos')?>'>Pedidos</a></li>
+            <li class="active"><a href='<?php echo site_url('backoffice/Opiniones')?>'>Opiniones</a></li>
+            <li class="active"><a href='<?php echo site_url('backoffice/lineapedidos')?>'>Linea pedido</a></li>
+            <li class="active"><a href='<?php echo site_url('backoffice/articulos')?>'>Articulos</a></li>
+            <li class="active"><a href='<?php echo site_url('backoffice/imagenes')?>'>Imagenes</a></li>
+            <li class="active"><a href='<?php echo site_url('backoffice/articulosusuario')?>'>Articulos usuario</a></li>
+          </ul>
+      </nav>
 			<?php echo $output; ?>
 		</div>
 	</div>
-
+  <br />
 </body>
 <?php
 	$this->load->view('inc/pie.php')
