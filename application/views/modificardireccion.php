de19265d07001568b63f7b69ba3b2d7d186cd933
davidmarquezherreros <davidmarquezherreros@gmail.com>
2017-01-26 10:50:31 +0100

    Comprobacion JS de las direcciones

--- application/views/modificardireccion.php
+++ application/views/modificardireccion.php
@@ -70,32 +70,113 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 					 <label for="postal">Codigo postal:</label>
 					 <?php
 								 if($operacion=="crear"){
-									 echo "<input type=\"text\" class=\"form-control\" name=\"postal\" placeholder=\"Escriba su codigo postal\" autofocus id=\"input_postal\"><br>";
+									 echo "<input type=\"number\" class=\"form-control\" name=\"postal\" placeholder=\"Escriba su codigo postal\" autofocus id=\"input_postal\"><br>";
 								 }
 								 else{
-									 echo "<input type=\"text\" class=\"form-control\" name=\"postal\" value=".$datos[0]->CodigoPostal." autofocus id=\"input_postal\"><br>";
+									 echo "<input type=\"number\" class=\"form-control\" name=\"postal\" value=".$datos[0]->CodigoPostal." autofocus id=\"input_postal\"><br>";
 								 }
 						?>
 					<label for="telefono">Telefono:</label>
 					<?php
 								if($operacion=="crear"){
-									echo "<input type=\"text\" class=\"form-control\" name=\"telefono\" placeholder=\"Escriba su telefono\" autofocus id=\"input_telefono\"><br>";
+									echo "<input type=\"number\" class=\"form-control\" name=\"telefono\" placeholder=\"Escriba su telefono\" autofocus id=\"input_telefono\"><br>";
 								}
 								else{
-									echo "<input type=\"text\" class=\"form-control\" name=\"telefono\" value=".$datos[0]->Telefono." autofocus id=\"input_telefono\"><br>";
+									echo "<input type=\"number\" class=\"form-control\" name=\"telefono\" value=".$datos[0]->Telefono." autofocus id=\"input_telefono\"><br>";
 								}
 					 ?>
 					<input id="btnModificar" type="submit" class="btn btn-default" value="Guardar datos"><br>
 				</div>
 			</form>
+			<div class="alert alert-danger" id="errorPais" style="min-width=480px; margin: auto; width: 50%;">
+				<strong>Error!</strong> Pais incorrecto!
+			</div>
+			<div class="alert alert-danger" id="errorCiudad" style="min-width=480px; margin: auto; width: 50%;">
+				<strong>Error!</strong> Ciudad incorrecta!
+			</div>
+			<div class="alert alert-danger" id="errorComunidad" style="min-width=480px; margin: auto; width: 50%;">
+				<strong>Error!</strong> Comunidad Autonoma incorrecta!
+			</div>
+			<div class="alert alert-danger" id="errorDireccion" style="min-width=480px; margin: auto; width: 50%;">
+				<strong>Error!</strong> Direccion incorrecta!
+			</div>
+			<div class="alert alert-danger" id="errorPostal" style="min-width=480px; margin: auto; width: 50%;">
+				<strong>Error!</strong> Codigo postal incorrecto!
+			</div>
+			<div class="alert alert-danger" id="errorTelefono" style="min-width=480px; margin: auto; width: 50%;">
+				<strong>Error!</strong> Telefono incorrecto!
+			</div>
 		</main>
   	</div>
 </div>
 </body>
 <script type="text/javascript">
-	$(document).ready(function(){
+	$(document).ready(function(e){
 		$("#input_id").hide();
-	});
+		$("#errorPais").hide();
+		$("#errorCiudad").hide();
+		$("#errorComunidad").hide();
+		$("#errorDireccion").hide();
+		$("#errorPostal").hide();
+		$("#errorTelefono").hide();
+		$("#btnModificar").click(function(e) {
+				var pais = $("#input_pais").val();
+				var ciudad = $("#input_ciudad").val();
+				var comunidad = $("#input_comunidad").val();
+				var direccion = $("#input_direccion").val();
+				var postal = $("#input_postal").val();
+				var telefono = $("#input_telefono").val();
+				if(pais == undefined || pais == ""){
+					e.preventDefault();
+					$("#errorPais").show();
+					$("#input_pais").addClass("has-error");
+				}
+				else{
+							$("#errorPais").hide();
+				}
+				if(ciudad == undefined || ciudad == ""){
+					e.preventDefault();
+					$("#errorCiudad").show();
+					$("#input_ciudad").addClass("has-error");
+				}
+				else{
+							$("#errorCiudad").hide();
+				}
+				if(comunidad == undefined || comunidad == ""){
+					e.preventDefault();
+					$("#errorComunidad").show();
+					$("#input_comunidad").addClass("has-error");
+				}
+				else{
+							$("#errorComunidad").hide();
+				}
+				if(direccion == undefined || direccion == ""){
+					e.preventDefault();
+					$("#errorDireccion").show();
+					$("#input_direccion").addClass("has-error");
+				}
+				else{
+							$("#errorDireccion").hide();
+				}
+				if(postal == undefined || postal == ""){
+					e.preventDefault();
+					$("#errorPostal").show();
+					$("#input_postal").addClass("has-error");
+				}
+				else{
+							$("#errorPostal").hide();
+				}
+				if(telefono == undefined || telefono == ""){
+					e.preventDefault();
+					$("#errorTelefono").show();
+					$("#input_Telefono").addClass("has-error");
+				}
+				else{
+							$("#errorTelefono").hide();
+				}
+			});
+		});
+
 </script>
 <?php
 	$this->load->view('inc/pie.php')
